85e244657d7c08dbc39fc166569559a9b04aa6aa
Joonas Tuomi <joonastapanituomi@gmail.com>
2025-11-19 18:29:18 +0200

    Integrated babylon/index.html into project

---
+++ apps/react-ui/src/pages/BabylonPage.tsx
@@ -0,0 +1,10 @@
+import { Babylon } from '../components/Babylon'
+
+export function BabylonPage(): JSX.Element {
+  return (
+    <>
+      <h1 className="text-4xl tracking-tight font-bold mb-8">BabylonJS Playground</h1>
+      <Babylon />
+    </>
+  )
+}
