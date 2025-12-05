5a0eba417d2c8feb370dd204649cf2f573a820c9
v-kuu <ville.m.kuusela@gmail.com>
2025-12-05 15:00:31 +0200

    Moved babylon ts to game island but now it loads forever

--- site/islands/Game.tsx
+++ site/islands/Game.tsx
@@ -1,3 +1,78 @@
+import * as BABYLON from "@babylonjs/core";
+import { useEffect } from "preact/hooks"
+
 export function Game() {
-  return <iframe src="/babylon.html" class="w-full h-screen border-0" />;
+	useEffect(() => {
+	const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
+	const engine = new BABYLON.Engine(canvas, true);
+
+	function createScene(): BABYLON.Scene
+	{
+		var scene = new BABYLON.Scene(engine);
+
+		//camera setup
+		var camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 6, 20, new BABYLON.Vector3(0, 0, 0), scene);
+		camera.attachControl(canvas, true);
+		camera.lowerBetaLimit = camera.upperBetaLimit = camera.beta;
+		camera.lowerAlphaLimit = camera.upperAlphaLimit = camera.alpha;
+		camera.panningAxis = new BABYLON.Vector3(1, 1, 0);
+		camera.panningSensibility = 1e3;
+
+		//light setup
+		const light = new BABYLON.DirectionalLight("light", new BABYLON.Vector3(-0.7, -1, -0.5), scene);
+		light.autoCalcShadowZBounds = true;
+		light.intensity = 0.7;
+
+		//entity setup
+		var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 0.5, segments: 32 }, scene);
+		sphere.position.y = 0.25;
+		var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 12, height: 6 }, scene);
+		ground.receiveShadows = true;
+		var box1 = BABYLON.MeshBuilder.CreateBox("player1", { width: 0.5, height: 0.3, depth: 3 }, scene);
+		box1.position.x = -6;
+		box1.position.y = 0.2;
+		var box2 = BABYLON.MeshBuilder.CreateBox("player2", { width: 0.5, height: 0.3, depth: 3 }, scene);
+		box2.position.x = 6;
+		box2.position.y = 0.2;
+
+		//shadow setup
+		const csm = new BABYLON.CascadedShadowGenerator(4096, light);csm.autoCalcDepthBounds = true;
+		csm.addShadowCaster(sphere);
+		csm.addShadowCaster(box1);
+		csm.addShadowCaster(box2);
+
+		//input setup
+		const keys = {};
+		window.addEventListener("keydown", (e) => keys[e.key] = true);
+		window.addEventListener("keyup", (e) => keys[e.key] = false);
+		const moveSpeed = 6;
+		scene.onBeforeRenderObservable.add(() => {
+			const delta = scene.getEngine().getDeltaTime() / 1e3;
+			const distance = moveSpeed * delta;
+			if (keys["w"]) {
+				box1.position.z += distance;
+			}
+			if (keys["s"]) {
+				box1.position.z -= distance;
+			}
+			if (keys["i"]) {
+				box2.position.z += distance;
+			}
+			if (keys["k"]) {
+				box2.position.z -= distance;
+			}
+		});
+		return scene;
+	};
+
+	const scene = createScene();
+	engine.runRenderLoop(() => scene.render());
+	window.addEventListener("resize", () => engine.resize());
+	
+	return () => {
+		window.removeEventListener("resize", () => engine.resize());
+		engine.dispose();
+	}
+	}, []);
+	return <canvas id="renderCanvas" class="w-full h-screen border-0" />;
 }
