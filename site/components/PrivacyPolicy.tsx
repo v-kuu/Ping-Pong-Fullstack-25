52895fa5dd89859e8f08dd78224ba094910dc066
Joonas Tuomi <jtuomi@student.hive.fi>
2025-12-11 17:34:34 +0200

    [site] Add Footer, Privacy Policy and ToS parts
    All the relevant components and island is first added.
    Content is still going to be added now here just for adding basic functionality.

---
+++ site/components/PrivacyPolicy.tsx
@@ -0,0 +1,8 @@
+export default function PrivacyPolicy() {
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            We do not collect any of your identifiable data nor use it in anyway.
+        </div>
+    )
+}
---
+++ site/components/ToS.tsx
@@ -0,0 +1,8 @@
+export default function ToS() {
+    return (
+        <div>
+            <h1>Terms of Service</h1>
+            Lorem ipsum doloret ami... ...
+        </div>
+    )
+}
