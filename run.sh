a24450a934d889955ba6bdc038616672b7ad94f8
Joonas Tuomi <joonastapanituomi@gmail.com>
2025-12-15 12:56:03 +0200

    Added script to run docker

--- run.sh
+++ run.sh
@@ -1,19 +1,56 @@
-#!/bin/sh
+#!/bin/bash
 
 Help()
 {
     echo "Available options:"
-    echo "h - Display this help text"
-    echo "b - Build the project using Docker"
-    echo "u - Build (if needed) and run container"
+    echo "-h  Display this help text"
+    echo "-b  Build the project using Docker"
+    echo "-u  Build (if needed) and run container"
+    echo "-d  Bring down all containers"
+    echo "-c  Remove all the containers and data"
 }
 
-while getopts ":h" option; do
+Build()
+{
+    docker compose build
+}
+
+Up()
+{
+    docker compose up -d
+}
+
+Down()
+{
+    docker compose down
+}
+
+Clean()
+{
+    docker compose down -v --remove-orphans
+    docker builder prune -af
+    docker image prune -af
+    docker volume prune -af
+}
+
+while getopts ":h :b :u :c :d" option; do
    case $option in
       h) # display Help
          Help
          exit;;
+      b)
+         Build
+         exit;;
+      u)
+         Up
+         exit;;
+      c)
+         Clean
+         exit;;
+      d)
+         Down
+         exit;;
+      \?)
+         echo "Valid options are h, b, u and c"
    esac
 done
-
-echo "Hello world!"
