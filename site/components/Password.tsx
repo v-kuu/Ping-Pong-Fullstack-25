bc61fcb04587a2197057d1240073f9d9c5482ef4
Joonas Tuomi <jtuomi@student.hive.fi>
2025-12-17 17:12:41 +0200

    Added signals from Preact in preparation for users

--- site/components/Password.tsx
+++ site/components/Password.tsx
@@ -6,7 +6,7 @@ export function UpdatePassword() {
       <div>
     <h2>Change your password</h2>
     <form method="POST" action="/update">
-      <label class="input" for="password">
+      <label class="input" for="oldPassword">
         Current Password:
       </label>
       <br/>
@@ -17,8 +17,8 @@ export function UpdatePassword() {
         pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
         title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
         placeholder="Your pass shall be validated"
-        id="password"
-        name="password"
+        id="oldPassword"
+        name="oldPassword"
         required
       />
       <div class="validator-hint">
@@ -28,7 +28,7 @@ export function UpdatePassword() {
         at least one uppercase letter
       </div>
       <br />
-      <label class="input" for="password">
+      <label class="input" for="newPassword">
         New Password:
       </label>
       <br/>
@@ -39,8 +39,8 @@ export function UpdatePassword() {
         pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
         title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
         placeholder="What should your password be?"
-        id="password"
-        name="password"
+        id="newPassword"
+        name="newPassword"
         required
       />
       <div class="validator-hint">
--- site/islands/Settings.tsx
+++ site/islands/Settings.tsx
@@ -1,34 +1,46 @@
 import { Userinfo } from "./Info.tsx";
 import { UpdatePassword } from "../components/Password.tsx";
 import { ChangeAvatar } from "./Avatar.tsx";
+import { signal } from '@preact/signals';
+import { Show } from '@preact-signals/utils/components';
+
+let isInfo = signal(true);
+let isPass = signal(false);
+let isAvatar = signal(false);
 
 export function Setting() {
   return (
-    <div className="drawer lg:drawer-open">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+    <div class="drawer lg:drawer-open">
+      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
       <div className="prose max-w-none drawer-content flex flex-col items-center justify-center">
+      <Show when={isInfo}>
         <Userinfo />
+      </Show>
+      <Show when={isPass}>
         <UpdatePassword />
+      </Show>
+      <Show when={isAvatar}>
         <ChangeAvatar />
-        <label htmlFor="my-drawer-3" className="btn drawer-button lg:hidden">
+      </Show>
+      <label htmlFor="my-drawer-3" class="btn drawer-button lg:hidden">
           Open drawer
-        </label>
+      </label>
       </div>
-      <div className="drawer-side">
+      <div class="drawer-side">
         <label
           htmlFor="my-drawer-3"
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 min-h-full w-80 p-4">
+        <ul class="menu bg-base-200 min-h-full w-80 p-4">
           <li>
-            <a>User information</a>
+            <button onClick={() => {isInfo.value = true; isPass.value = false; isAvatar.value = false;}}>User information</button>
           </li>
           <li>
-            <a>Password</a>
+            <button onClick={() => {isPass.value = true; isInfo.value = false; isAvatar.value = false;}}>Password</button>
           </li>
           <li>
-            <a>Avatar</a>
+            <button onClick={() => {isAvatar.value = true; isPass.value = false; isInfo.value = false;}}>Avatar</button>
           </li>
         </ul>
       </div>
