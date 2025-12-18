0fc19aa947e46f655cde56951f4e959e12880812
datagore <104622881+datagore@users.noreply.github.com>
2025-12-18 22:17:38 +0200

    Initial setup for Web3D part

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
+import wasm from "vite-plugin-wasm"
+import { watchAndRun } from 'vite-plugin-watch-and-run'
+import path from 'node:path'
 
 export default defineConfig({
   root: "./",
-  plugins: [fresh(), tailwindcss()],
+  plugins: [fresh(), tailwindcss(), wasm(),
+    watchAndRun([{
+      name: 'web3d',
+      watch: path.resolve('./web3d/*.c'),
+      run: 'make -C web3d --no-print-directory'
+    }])
+  ],
 });
