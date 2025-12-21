3ce8faa42fecb607bb653310956fcaaf26b32cfe
oliynykmax <olejnikmax@gmail.com>
2025-12-21 18:27:31 +0200

    astro fix

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
 export default defineConfig({
   integrations: [preact()],
-  output: 'server',
-  publicDir: 'static',
+  output: "server",
+  publicDir: "static",
   server: {
     port: 3000,
   },
   vite: {
     css: {
       postcss: {
-        plugins: [require('@tailwindcss/postcss')],
+        plugins: [require("@tailwindcss/postcss")],
       },
     },
   },
