e4b81e86a5dace882edca5650bb8b083955b89be
Joonas Tuomi <jtuomi@student.hive.fi>
2025-12-22 21:38:35 +0200

    Deleted empty island and made Footer horizontal

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwindcss from "@tailwindcss/vite";
 import bun from "@nurodev/astro-bun";
+
+process.env.ASTRO_TELEMETRY_DISABLED = '1';
+
 export default defineConfig({
   integrations: [preact()],
   output: "server",
