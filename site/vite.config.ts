36bc34d021da82deabe1c8a2d0c85ab4ddfac2b7
Joonas Tuomi <jtuomi@student.hive.fi>
2025-12-22 15:45:55 +0200

    Created own vite plugin to compile the game on build

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -3,6 +3,17 @@ import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
 import wasm from "vite-plugin-wasm";
 import { run } from "vite-plugin-run";
+import { exec } from "node:child_process";
+
+const make = () => {
+	return {
+		name: "make-plugin",
+        apply: "build",
+		buildStart: () => {
+			exec('make -sC web3d', (_, _output, _error) => {});
+		},
+	};
+};
 
 export default defineConfig({
   root: "./",
@@ -17,6 +28,7 @@ export default defineConfig({
     fresh(),
     tailwindcss(),
     wasm(),
+    make(),
   ],
   build: {
     target: "esnext",
