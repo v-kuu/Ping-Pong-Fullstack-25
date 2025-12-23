7ccfb5650de9cbbebf42fbf1d7ae411419b6f6fa
oliynykmax <olejnikmax@gmail.com>
2025-12-23 02:05:20 +0200

    different small import fixes, dockerfile fixed for the web3d

--- site/islands/Footer.tsx
+++ site/islands/Footer.tsx
@@ -1,9 +1,4 @@
-import PrivacyPolicy from "../components/PrivacyPolicy.tsx";
-import ToS from "../components/ToS.tsx";
-
 export default function Footer(props: { active: string }) {
-    const isPP = props.active == "/privacy-policy";
-    const isToS = props.active == "/terms-of-service";
     const items = [
         {
             name: "Terms of Service",
@@ -17,25 +12,24 @@ export default function Footer(props: { active: string }) {
     return (
         <footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
             <aside class="menu menu-horizontal">
-               <p>Copyright © {new Date().getFullYear()} - All right reserved by Pong Company</p>
-               <ul class="menu menu-horizontal">
-                  {items.map((item) => (
-               <li
-                  key={item.href}
-                  aria-current={props.active === item.href ? "page" : undefined}
-               >
-                 <a
-                     href={item.href}
-                     class ={`p-1 sm:p-2 ${
-                      props.active === item.her ? "font-bold" : ""
-                     } aria-[current]:font-bold`}
-                  >
-                    {item.name}
-                </a>
-              </li>
-             ))}
-           </ul>
-          </aside>
-       </footer>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved by Pong Company</p>
+                <ul class="menu menu-horizontal">
+                    {items.map((item) => (
+                        <li
+                            key={item.href}
+                            aria-current={props.active === item.href ? "page" : undefined}
+                        >
+                            <a
+                                href={item.href}
+                                class={`p-1 sm:p-2 ${props.active === item.href ? "font-bold" : ""
+                                    } aria-[current]:font-bold`}
+                            >
+                                {item.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </aside>
+        </footer>
     )
 }
