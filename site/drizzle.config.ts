2a3c187b984d48c20f5f79516f944330c0889662
oliynykmax <olejnikmax@gmail.com>
2025-12-23 17:04:12 +0200

    basic drizzle-orm setup + example usage file

---
+++ site/drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './db/schema.ts',
+  dialect: 'sqlite',
+  dbCredentials: {
+    url: './db/app.db',
+  },
+});
