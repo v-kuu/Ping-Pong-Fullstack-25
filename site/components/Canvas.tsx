1c994beded809f844f4df163312d5a94e794c039
Joonas Tuomi <joonastapanituomi@gmail.com>
2025-12-29 18:20:59 +0200

    Removed forwardRef

--- site/components/Canvas.tsx
+++ site/components/Canvas.tsx
@@ -1,12 +1,3 @@
-import { forwardRef } from "preact/compat";
-
-export const Canvas = forwardRef<HTMLCanvasElement>((props, ref) => {
-    return (
-      <canvas
-        id="renderCanvas"
-        ref={ref}
-        class="w-full h-screen border-0"
-        {...props}
-      />
-     );
-});
+export function Canvas() {
+  return <canvas id="renderCanvas" class="w-full h-screen border-0" />;
+}
