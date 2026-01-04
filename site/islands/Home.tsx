e1580ae8cf1c1efc9b8776ebaf996de4b8cc1897
jtuomi <jtuomi@student.hive.fi>
2026-01-04 04:30:16 +0200

    Slightly tweaked styles and added client:load to Header for theme toggling.

--- site/islands/Home.tsx
+++ site/islands/Home.tsx
@@ -3,11 +3,11 @@ import { Button } from "../components/Button.tsx";
 export function Home() {
   return (
     <div class="object-fill flex flex-col items-center justify-center">
-      <div class="prose max-w-none items-center">
+      <div class="prose prose-slate max-w-none">
         <h1>Hi ya there</h1>
         <p>Welcome, we have games.</p>
       </div>
-      <div class="menu">
+      <div class="menu gap-1">
         <a href="/pong">
           <img src="/pong.gif" alt="Pong Game" />
         </a>
@@ -17,13 +17,14 @@ export function Home() {
         </a>
         <Button>Play Web3D</Button>
         <br />
-        <div class="w-full flex justify-center">
+        <div class="w-full flex justify-center gap-10">
           <a href="/login">
             <Button>Login</Button>
           </a>
           <a href="/signup">
             <Button> Register </Button>
           </a>
+          <p>Enjoy !</p>
         </div>
       </div>
     </div>
