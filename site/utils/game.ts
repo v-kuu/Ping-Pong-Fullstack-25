d6dc686669c9347193e2eece356dfc12eb3c02f8
jtuomi <jtuomi@student.hive.fi>
2026-01-04 04:01:37 +0200

    Removed unused files.

--- site/utils/game.ts
+++
@@ -1,7 +0,0 @@
-import { Signal, signal } from "@preact/signals";
-
-var exportedSignal: Signal<number | undefined>;
-
-exportedSignal = signal<number | undefined>(undefined);
-
-export default exportedSignal;
