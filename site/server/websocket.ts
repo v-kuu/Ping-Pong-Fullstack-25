29c6a07a70378f7590a3e75c3c50bc0ed7d994bd
Vallu <vallu9@hotmail.com>
2026-01-06 11:40:33 +0200

    feat: astro example page testing with multiple clients

--- site/server/websocket.ts
+++ site/server/websocket.ts
@@ -1,26 +1,30 @@
 // server/websocket.ts
+import type { ServerWebSocket } from "bun";
+
+
+const clients = new Set<ServerWebSocket<unknown>>();
 
 Bun.serve({
     port: 3001,
     fetch(req, server) {
-        // upgrade the request to a WebSocket
-        if (server.upgrade(req)) {
+        const url = new URL(req.url)
+
+        if (url.pathname === "/ws" && server.upgrade(req)) {
             return;
         }
         return new Response("WebSocket server");
     },
     websocket: {
-        message(ws, message) {
-            console.log("Received", message)
-            ws.send("Hello clinet");
-        },
+        message(ws) {},
         open(ws) {
-            console.log("Client connected")
+            clients.add(ws)
+            console.log("Client connected. Total:", clients.size)
         },
-        close(ws, code, message) {
-            console.log("Client disconnected")
+        close(ws) {
+            clients.delete(ws)
+            console.log("Client disconnected. Total:", clients.size)
         },
     },
 });
 
-console.log("WebSocket server running on ws://localhost:3001");
+console.log("WebSocket server running on http://localhost:3001/ws");
