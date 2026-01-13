6d47012b309bfcafd95064b8a83cf4c8b042fe43
Ville Kuusela <vkuusela@student.hive.fi>
2026-01-13 13:42:24 +0200

    Fixed server side countdown probably

--- site/utils/server/babylon_serverstates.ts
+++ site/utils/server/babylon_serverstates.ts
@@ -15,18 +15,14 @@ export function setState(newState: GameState, scene: Scene)
 	{
 		case GameState.Countdown:
 			Globals.playing = false;
-			//send countdown state to clients
-			// somehow count down the 3 2 1 GO in server
-			for (let i = 0; i < 3; i++)
+			Tools.DelayAsync(3000).then(() =>
 			{
-				Tools.DelayAsync(1000).then();
-			}
-			setState(GameState.Playing, scene);
+				setState(GameState.Playing, scene);
+			});
 			break ;
 
 		case GameState.Playing:
 			Globals.playing = true;
-			//send playing state to clients
 			break ;
 	}
 }
